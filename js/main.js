8875e0130d88856bdf348140b0560a996e5077b2
Sebastian Scarpa <sebastian.scarpa@gmail.com>
2014-12-30 17:43:14 +0100

    final toutches

--- js/main.js
+++ js/main.js
@@ -7,27 +7,7 @@
         verticalScrolling: true
     });
 
-
-    // function scrollToAnchor(aid){
-    // var aTag = $("section[name='"+ aid +"']");
-    // $('html,body').animate({scrollTop: aTag.offset().top},'slow');
-    // }
-
-    // $("#section1").click(function() {
-    //    scrollToAnchor('1');
-    // });
-    // $("#section2").click(function() {
-    //    scrollToAnchor('2');
-    // });
-    // $("#section3").click(function() {
-    //    scrollToAnchor('3');
-    // });
-    // $("#section4").click(function() {
-    //    scrollToAnchor('4');
-    // });
-
+    smoothScroll.init();
 
 })();
-
-smoothScroll.init();
 
